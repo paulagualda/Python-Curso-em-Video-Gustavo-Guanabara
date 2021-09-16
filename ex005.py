2d73d85aba0816394e5940334dbf62b66896dc92
Paula Gualda <61845763+paulagualda@users.noreply.github.com>
2021-09-16 14:28:09 -0300

    PythonExercícios-Init

---
+++ ex005.py
@@ -0,0 +1,9 @@
+n = int(input('Um valor:'))
+s = n + 1
+a = n -1
+print('O sucessor de {} é {} e o antecessor é {}'.format(n,s,a))
+
+# ou sem com somente uma variável
+#print(' O sucessor de {} é {} e o antecessor é {}'.format(n,(n+1), (n+2)))
+#dessa maneira você economiza memória
+
---
+++ ex007.py
@@ -0,0 +1,4 @@
+n1 = float(input('Nota 1:'))
+n2 = float(input('Nota 2:'))
+m = (n1 + n2)/2  # () utilizado para ordem de precedência
+print('A média das suas notas é {:.1f}'.format(m)) # :.1f depois do ponto flutuante coloque 1 dígito
